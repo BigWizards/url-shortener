7e00e6b8f5a87318e7cdcbd2680d068916b80375
BigWizards <95jsjones@gmail.com>
2021-11-02 11:19:59 +0000

    Add validation to url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,4 @@
 class Url < ApplicationRecord
+  validates :long_url, presence: true, format: { with: ^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix,
+    message: "Only allows full urls" }
 end
