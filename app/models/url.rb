01dfedc37f3e11e5d874e1cc3a53b1e91c5c909e
BigWizards <95jsjones@gmail.com>
2021-11-03 17:52:21 +0000

    Fix long_url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,4 @@
 class Url < ApplicationRecord
-  validates :long_url, presence: true, format: { with: ^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix,
-    message: "Only allows full urls" }
+  validates :long_url, presence: true, :format => { :with => URI::regexp(%w(http https)), 
+    :message => "Valid URL required"}
 end
