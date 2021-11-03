1c50fe008a64a90978a50d301e5f85e4e85c1ca6
BigWizards <95jsjones@gmail.com>
2021-11-03 17:51:38 +0000

    Add methods to urls_controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,9 +3,40 @@ class UrlsController < ApplicationController
     @url = Url.new
   end
 
-  def create
+  def show
+    @url = Url.new
+    @urls = Url.all.order(created_at: :desc)
   end
 
-  def convert_url
+  def create
+    @url = Url.new(url_params)
+    @url.short_url = url_generator
+
+    respond_to do |format|
+      if @url.save
+        format.html { redirect_to @url, notice: "Here is your new url." }
+        format.json { render :show, status: :created, location: @url }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @url.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  
+  def url_generator
+    url = "g.et/"
+    url << "#{random_characters}" 
+    Url.where(short_url: url).exists? ? url_generator : url
+  end
+
+  def random_characters
+    charset = Array('0'..'9') + Array('a'..'z')
+    Array.new(6) { charset.sample }.join
+  end
+
+  def url_params
+    params.require(:url).permit(:long_url)
   end
 end
